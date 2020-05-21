25a39904e14b0ab9b6fe62780a6c2092e8f510cf
Justin Corbin <jcorbin2290@gmail.com>
2020-05-21 12:03:25 -0600

    Clear state

--- src/Components/ReservationMenu/ReservationMenu.js
+++ src/Components/ReservationMenu/ReservationMenu.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './ReservationMenu.css'
 
 class ReservationMenu extends Component {
   constructor(props) {
@@ -39,16 +40,23 @@ class ReservationMenu extends Component {
   clickHandler = (event) => {
     event.preventDefault()
     this.props.createReservation(this.state)
+    this.setState({
+        date: '',
+        id: null,
+        name: '',
+        number: 0,
+        time: ''
+    })
   }
 
   render() {
     return (
       <form className='reservationForm'>
-        <input className='userDate' type='text' placeholder='Date' onChange={this.updateDate}></input>
-        <input className='userName' type='text' placeholder='Name' onChange={this.updateName}></input>
-        <input className='userNumber' type='text' placeholder='Number of Guests' onChange={this.updateGuests}></input>
-        <input className='userTime' type='text' placeholder='Time' onChange={this.updateTime}></input>
-        <button className="submitReservation" type='click' onClick={this.clickHandler}>Make Reservation</button>
+        <input className='userDate catchAll' type='text' placeholder='Date' onChange={this.updateDate}></input>
+        <input className='userName catchAll' type='text' placeholder='Name' onChange={this.updateName}></input>
+        <input className='userNumber catchAll' type='text' placeholder='Number of Guests' onChange={this.updateGuests}></input>
+        <input className='userTime catchAll' type='text' placeholder='Time' onChange={this.updateTime}></input>
+        <button className="submitReservation catchAll" type='click' onClick={this.clickHandler}>Make Reservation</button>
       </form>
     );
   }
